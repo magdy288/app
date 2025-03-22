91a464c7479de7af681fe25fbf5cee0c5562de1d
magdy288 <magdy288m@gmail.com>
2025-03-22 02:01:14 +0200

    changes

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import PyPDF2
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 
 file = st.file_uploader('upload', type='pdf')
 
 if file is not None:
-    temp_file = f'./{file.name}.pdf'
+    temp_file = './temp.pdf'
     with open(temp_file, 'wb') as f:
         f.write(file.getvalue())
         file_name = file.name
